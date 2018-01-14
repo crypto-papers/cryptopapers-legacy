9a52bffaae9f201b699ec772776676fb160b8910
Marek <marekrewers@gmail.com>
2018-01-14 15:56:48 -0500

    Polishing up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import hodl from '../assets/hodl.gif'
+
 const NotFoundPage = () => (
   <div>
     <h3>OOPS...PAGE NOT FOUND</h3>
-    <p>You just hit a route that doesn&#39;t exist, please hodl with us.</p>
-    <img className='hodl_gif' src={hodl} height='600px' />
+    <p>It seems that the page you're looking for does not exist. Please hodl on while we resolve the problem.</p>
+    <div className='gif_container'>
+      <img className='hodl_gif' src={hodl} alt='404 Error' />
+    </div>
   </div>
 )
 
