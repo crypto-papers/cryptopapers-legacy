8da5dc0fbfaa21133ca9d821476438347164f361
Marek <marekrewers@gmail.com>
2018-01-15 22:47:40 -0500

    Adding documentation page

---
+++ src/pages/docs.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import download_cloud from '../assets/download_cloud.svg'
+
+const DocsPage = ({data}) => (
+  <div>
+    <p>CryptoPapers is a simple online archive that lists papers related to cryptocurrency. The archive includes cryptocurrency whitepapers, research papers, and technical documentation and is intended for educational purposes. This site does not provide investment advice nor does a paper's inclusion in the archive imply an endorsement of it's contents.</p>
+    <div className='paper_grid'>
+      <h3 className='grid_header'>Technical Documentation:</h3>
+      <div className='grid_wrapper'>
+        {data.allMarkdownRemark.edges.map(post => (
+          <div className='grid_item' id={post.node.frontmatter.path} key={post.node.id}>
+            <Link className='thumbnail_link' to={'/' + post.node.frontmatter.path}>
+              <img className='grid_thumbnail' src={'/covers/' + post.node.frontmatter.cover} /><br />
+            </Link>
+            <div className='meta_container'>
+              <div className='grid_link_container' >
+                <Link className='grid_link' to={'/' + post.node.frontmatter.path}>
+                  <p className='grid_link_text'>{post.node.frontmatter.title}</p>
+                </Link>
+              </div>
+              <div className='date_and_download_container'>
+                <p className='publication_date' >Published: {post.node.frontmatter.date_published}</p>
+                <Link className='grid_download' to={'/pdf/' + post.node.frontmatter.pdf}>
+                  <img className='download_icon' src={download_cloud} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+)
+
+export const pageQuery = graphql`
+  query DocsQuery {
+    allMarkdownRemark(
+      sort: { fields: [ frontmatter___date_added ], order: DESC }
+      filter: { frontmatter: { category: { eq: "documentation" } } }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+            cover
+            pdf
+            category
+            is_currency
+            currency
+            date_published
+            date_added
+          }
+        }
+      }
+    }
+  }
+`
+
+export default DocsPage
