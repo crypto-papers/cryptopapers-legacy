3d635a37c292c13833000f96795c7aa40c8e5c15
Marek <marekrewers@gmail.com>
2018-01-21 21:07:12 -0500

    Alter footer adding mail link and brave browser link

--- src/components/navigation/social_links.js
+++ src/components/navigation/social_links.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import twitter from '../../assets/twitter.svg'
 import github from '../../assets/github.svg'
+import mail from '../../assets/mail.svg'
+import brave from '../../assets/brave_icon.png'
 
 const SocialLinks = () => (
   <div className='contact_icons'>
@@ -10,6 +12,12 @@ const SocialLinks = () => (
     </a>
     <a className='github_link' href='https://github.com/mrewers/Cryptopapers' target='_blank'>
       <img className='github_icon' src={github} />
+    </a>
+    <a className='mail_link' href='mailto:cryptopapers@protonmail.com'>
+      <img className='mail_icon' src={mail} />
+    </a><br /><br />
+    <a className='brave_link' href='https://brave.com/' target='_blank'>
+      <p className='brave_text'>Supported by:</p> <img className='brave_icon' src={brave}/>
     </a>
   </div>
 )
